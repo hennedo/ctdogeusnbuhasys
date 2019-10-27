c28b87ef7fe145d496aa072772c5acd6ad812154
henne <hendrik@hfellerhoff.de>
2019-10-27 22:03:30 +0100

    sone frontend changes :)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/products',
+    redirectTo: '/users',
     pathMatch: 'full'
   },
   {
     path: 'products',
     loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   }
 ];
 
--- frontend/src/app/user/user-routing.module.ts
+++ frontend/src/app/user/user-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from './index/index.component';
+import {BuyComponent} from './buy/buy.component';
+import {CreateComponent} from './create/create.component';
+import {AddfundsComponent} from './addfunds/addfunds.component';
+import {LogComponent} from './log/log.component';
+import {UpdateComponent} from './update/update.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent
+  },
+  {
+    path: ':id/buy',
+    component: BuyComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: ':id/addfunds',
+    component: AddfundsComponent
+  },
+  {
+    path: ':id/log',
+    component: LogComponent
+  },
+  {
+    path: ':id/update',
+    component: UpdateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
